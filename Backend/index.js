66048065e74ed576af1c918977b4c0167adc6783
Riddhi-Patel16 <riddhi842004@gmail.com>
2024-07-07 16:40:47 +0530

    Backend api is readdy

---
+++ Backend/index.js
@@ -0,0 +1,126 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+import multer from 'multer';
+import path from 'path';
+import cors from 'cors'
+import dotenv from 'dotenv';
+
+dotenv.config();
+const port = process.env.PORT || 4000;
+let app=express();
+app.use(express.urlencoded({
+    extended:false
+}));
+app.use(cors());
+
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+});
+app.get('/',(req,res)=>{
+    res.send("Express is running")
+})
+
+const storage=multer.diskStorage({
+    destination:'./upload/images',
+    filename:(req,file,cb)=>{
+        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+
+const Product=mongoose.model("Product",{
+    id:{
+        type:Number,
+        required:true,
+    },
+    name:{
+        type:String,
+        required:true,
+    },
+    image:{
+        type:String,
+        required:true,
+    },
+    category:{
+        type:String,
+        required:true,
+    },
+    new_price:{
+        type:Number,
+        required:true,
+    },
+    old_price:{
+        type:Number,
+        required:true,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    },
+    avialable:{
+        type:Boolean,
+        default:true,
+    },
+})
+
+app.post('/addproduct',async(req,res)=>{
+    let products=await Product.find({});
+    let id;
+    if(products.length>0)
+    {
+        let last_product_object=products.slice(-1);
+        let last_id=last_product_object[0];
+        id=last_id+1;
+    }
+    else
+    id=1;
+    const product=new Product({
+        id:id,
+        name:req.body.name,
+        image:req.body.image,
+        category:req.body.category,
+        new_price:req.body.new_price,
+        old_price:req.body.old_price,
+    })
+    console.log(product);
+    await product.save();
+    console.log("Saved");
+    res.json({
+        success:1,
+        name:req.body.name,
+    })
+})
+
+app.post('/removeproduct',async(req,res)=>{
+    await Product.findOneAndDelete({id:req.body.id});
+    console.log("removed");
+    res.json({
+        success:1,
+        name:req.body.id
+    })
+})
+
+app.get('/allproducts',async(req,res)=>{
+    let products=await Product.find({});
+    console.log("Allproduct fetched");
+    res.json({
+        success:1,
+        products:products
+    })
+})
+
+const upload=multer({storage:storage})
+app.use('/images',express.static('upload/images'))
+app.post("/upload",upload.single('product'),(req,res)=>{
+    res.json({
+        success:1,
+        image_url:`http://localhost:${port}/images/${req.file.filename}`
+    })
+})
+
+
+app.listen(port,()=>{
+    console.log("Server is running on port 4000");
+})
